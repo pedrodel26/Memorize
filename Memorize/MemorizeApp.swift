0f6c16891e236465f1dda900b8492bf5af48d58f
pedrodel26 <pedromodesto26@gmail.com>
2024-03-14 10:51:57 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Pedro Delmondes  on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
